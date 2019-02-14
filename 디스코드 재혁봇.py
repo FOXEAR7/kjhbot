9f7a88e244a97e4ad8b06141d6200331ab641a62
kim20070407 <47625361+kim20070407@users.noreply.github.com>
2019-02-14 15:06:01 +0700

    Add files via upload

---
+++ 디스코드 재혁봇.py
@@ -0,0 +1,27 @@
+import discord
+import asyncio
+
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    print("login")
+    print(client.user.name)
+    print(client.user.id)
+    print("------------------")
+    await client.change_presence(game=discord.Game(name='디스코드봇 대화중', type=1))
+
+
+@client.event
+async def on_message(message):
+    if message.content.startswith("재혁아! 안녕"):
+        await client.send_message(message.channel, "안녕하세욧")
+
+@client.event
+async def on_message(message):
+    if message.content.startswith("재혁아! 나간다"):
+        await client.send_message(message.channel, "안녕히가세욧")
+
+
+client.run('NTQ1NDgxOTQyNzI0OTAyOTE3.D0aTUA.8EDBljbGSGvNjW9ZczLILMqeBJ8')
